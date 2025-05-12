55a871f7a5d92abab7d0995fdd9737ceb92352b7
HFloresID <hflores@ideasdigitales.cl>
2025-05-12 17:00:46 -0400

    add models

---
+++ app/Models/Product.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'description',
+        'category_id',
+        'subcategory_id',
+        'brand_id',
+        'sku',
+        'status',
+    ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function subcategory()
+    {
+        return $this->belongsTo(Subcategory::class);
+    }
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function prices()
+    {
+        return $this->hasMany(Price::class);
+    }
+}
