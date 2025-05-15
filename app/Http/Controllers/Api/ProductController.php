3afc3c3e4f1a58e2aad281d3224fc34f2618f249
HFlores <hflores@ideasdigitales.cl>
2025-05-15 10:31:43 -0400

    deleting comment code

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -18,36 +18,4 @@ class ProductController extends Controller
         return $product;
     }
 
-    // public function store(Request $request)
-    // {
-    //     $data = $request->validate([
-    //         'name' => 'required|string',
-    //         'description' => 'nullable|string',
-    //         'subcategory_id' => 'required|exists:subcategories,id',
-    //         'brand_id' => 'required|exists:brands,id',
-    //     ]);
-
-    //     return Product::create($data);
-    // }
-
-    // public function update(Request $request, Product $product)
-    // {
-    //     $data = $request->validate([
-    //         'name' => 'sometimes|string',
-    //         'description' => 'nullable|string',
-    //         //'slug' => 'sometimes|string|unique:products,slug,' . $product->id,
-    //         'subcategory_id' => 'sometimes|exists:subcategories,id',
-    //         'brand_id' => 'sometimes|exists:brands,id',
-    //     ]);
-
-    //     $product->update($data);
-
-    //     return $product;
-    // }
-
-    // public function destroy(Product $product)
-    // {
-    //     $product->delete();
-    //     return response()->noContent();
-    // }
 }
