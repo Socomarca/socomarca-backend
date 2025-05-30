a06c988246d8642d7c839106bac20273c6a0eddd
HFlores <hflores@ideasdigitales.cl>
2025-05-30 18:38:42 -0400

    finalizando carrito

--- app/Helpers/TotalHelper.php
+++ app/Helpers/TotalHelper.php
@@ -7,30 +7,6 @@ use App\Models\Product;
 
 class TotalHelper
 {
-    // public static function total($carrito)
-    // {
-    //     $totalProductos = [];
-    //     $totalCarrito = 0;
-
-    //     foreach ($carrito as $key => $producto) {
-
-    //         $productId = $producto['id'];
-    //         $product = Product::where('id', $productId)->first();
-    //         $priceObj = Price::where('id', $product->price_id)->where('is_active', 1)->first();
-    //         $price = $priceObj->price;
-            
-    //         $total = $producto['quantity'] * $price;
-    //         array_push($totalProductos, $total);
-    //     }
-
-    //     dd($totalProductos);
-    //     foreach ($totalProductos as $value) {
-    //         $totalCarrito += $value;
-    //     }
-
-    //     return $totalCarrito;
-    // }
-
     public static function totalCarrito($carrito)
     {
 
