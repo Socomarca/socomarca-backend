84f576881f8c5ecf829e510fc0cffb83ac212198
Alexis <asoto@ideasdigitales.cl>
2025-06-05 16:57:12 -0400

    fix: en el store estaba sumando segun la cantidad de productos

--- app/Http/Controllers/Api/CartItemController.php
+++ app/Http/Controllers/Api/CartItemController.php
@@ -26,7 +26,7 @@ class CartItemController extends Controller
             ->first();
 
         if ($item) {
-            $item->quantity += $data['quantity'];
+            $item->quantity = $data['quantity'];
             $item->save();
         } else {
             $item = new CartItem;
