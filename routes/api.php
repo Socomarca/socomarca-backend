69c412501c3bc9b5e35e34fdd1e88b7ffa2900ca
Alexis <asoto@ideasdigitales.cl>
2025-06-05 17:40:54 -0400

    feat: agregar ruta para validar token

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,14 @@ use App\Http\Controllers\Api\PaymentMethodController;
 use App\Http\Controllers\Api\OrderController;
 use App\Http\Controllers\Api\WebpayController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+Route::middleware('auth:sanctum')->get('/auth/check-token', function (Request $request) {
+    return response()->json([
+        'valid' => true,
+        'user' => $request->user(),
+    ]);
+});
 
 Route::prefix('auth')->group(function () {
     Route::post('/token', [AuthController::class, 'login'])->name('auth.token.store');
@@ -30,6 +35,9 @@ Route::prefix('auth')->group(function () {
         Route::post('/restore', [PasswordResetController::class, 'forgotPassword'])->name('auth.password.restore');
     });
     Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/check-token', function () {
+            return response()->json(['valid' => true]);
+        })->name('auth.token.check');
         Route::delete('/token', [AuthController::class, 'destroy'])->name('auth.token.destroy');
         Route::prefix('/password')->group(function () {
             Route::put('', [PasswordResetController::class, 'changePassword'])->name('password.update');
