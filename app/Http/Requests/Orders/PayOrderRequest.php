121a06b59f72b6577e7fe5c494298898b09391c2
JavierAgueroCL <javier.aguero@bengala.cl>
2025-06-12 16:51:04 -0400

    refactor: #122 - Unificar endpoints de pago de la orden

--- app/Http/Requests/Orders/PayOrderRequest.php
+++ app/Http/Requests/Orders/PayOrderRequest.php
@@ -14,19 +14,12 @@ class PayOrderRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'order_id' => 'required|exists:orders,id',
-            'user_id' => 'required|exists:users,id|integer'
         ];
     }
 
     public function messages(): array
     {
         return [
-            'order_id.required' => 'El ID de la orden es requerido',
-            'order_id.exists' => 'La orden no existe',
-            'user_id.required' => 'El ID del usuario es requerido',
-            'user_id.exists' => 'El usuario no existe',
-            'user_id.integer' => 'El ID del usuario debe ser un número entero'
         ];
     }
 
