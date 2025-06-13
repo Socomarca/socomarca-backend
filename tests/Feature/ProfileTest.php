46f3cd1a79447c3990856908cd672edea1f21512
Samuel Alderete Rayón <sammycoder@outlook.com>
2025-06-13 14:18:10 -0600

    Adds user profile endpoint
    Adds a new endpoint to retrieve the authenticated user's profile information.
    
    This includes the user's basic details, billing address, and default shipping address. A new ProfileResource is created to format the response.

---
+++ tests/Feature/ProfileTest.php
@@ -0,0 +1,75 @@
+<?php
+
+use App\Models\Address;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Testing\Fluent\AssertableJson;
+
+uses(RefreshDatabase::class);
+
+test('usuario puede ver la información de su propio perfil', function () {
+    $user = User::factory()
+        ->has(
+            Address::factory([
+                'type' => 'shipping',
+                'is_default' => 1,
+            ])->count(1)
+        )->has(
+            Address::factory([
+                'type' => 'billing',
+                'is_default' => 1,
+            ])->count(1)
+        )->has(
+            Address::factory([
+                'type' => 'shipping',
+                'is_default' => 0,
+            ])->count(2)
+        )->create();
+
+    $addressStructure = [
+            'id',
+            'address_line1',
+            'address_line2',
+            'postal_code',
+            'is_default',
+            'type',
+            'phone',
+            'contact_name',
+            'municipality' => [
+                'id',
+                'name',
+                'code',
+                'region' => [
+                    'id',
+                    'name',
+                    'code',
+                ]
+            ],
+            'created_at',
+            'updated_at',
+        ];
+
+    $structure = [
+        "rut",
+        "name",
+        "business_name",
+        "email",
+        "phone",
+        "is_active",
+        "billing_address" => $addressStructure,
+        "default_shipping_address" => $addressStructure,
+    ];
+
+    $this->actingAs($user, 'sanctum')
+        ->getJson('/api/profile')
+        ->assertStatus(200)
+        ->assertJsonStructure($structure)
+        ->assertJsonFragment(['rut' => $user->rut])
+        ->assertJson(fn (AssertableJson $json) =>
+            $json->where('rut', $user->rut)
+                ->where('name', $user->name)
+                ->where('billing_address.id', $user->billing_address->id)
+                ->where('default_shipping_address.id', $user->default_shipping_address->id)
+                ->etc()
+        );;
+});
